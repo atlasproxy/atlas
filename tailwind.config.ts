27fe4e41376619df15d09ee8a138018d3c764611
proudparrot2 <knowing.discos0e@icloud.com>
2024-07-01 10:54:42 -0500

    Go back to old theme with more rounding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
-import daisythemes from "daisyui/src/theming/themes"
+import daisyui from 'daisyui'
+import daisythemes from 'daisyui/src/theming/themes'
 import typography from '@tailwindcss/typography'
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	daisyui: {
-		themes: [{
-			sunset: {
-				...daisythemes["sunset"],
-				primary: "#38BDF9",
-			},
-		},],
-		logs: false
-	},
-	plugins: [daisyui, typography],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  daisyui: {
+    themes: [
+      {
+        atlas: {
+          ...daisythemes['night'],
+          '--rounded-box': '1.2rem',
+          '--rounded-btn': '0.8rem',
+          '--rounded-badge': '0.4rem',
+          '--tab-radius': '0.7rem'
+        }
+      }
+    ],
+    logs: false
+  },
+  plugins: [daisyui, typography]
 }
